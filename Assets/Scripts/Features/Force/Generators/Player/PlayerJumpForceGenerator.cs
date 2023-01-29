c237c3dc581f477c367876fe3ca2db09d43ab0da
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2023-01-29 09:46:35 +0200

    Make player able to jump only if is grounded

--- Assets/Scripts/Features/Force/Generators/Player/PlayerJumpForceGenerator.cs
+++ Assets/Scripts/Features/Force/Generators/Player/PlayerJumpForceGenerator.cs
@@ -1,5 +1,6 @@
 using System;
 using WizardSpells.Data.Configuration.Player;
+using WizardSpells.Data.Dynamic;
 using WizardSpells.Features.Force.Accumulators;
 using WizardSpells.Services.Input;
 using Zenject;
@@ -11,18 +12,26 @@ namespace WizardSpells.Features.Force.Generators.Player
         private readonly IJumpingInputService _jumpingInputService;
         private readonly IPlayerConfig _config;
         private readonly IPermanentForceAccumulator _forceAccumulator;
+        private readonly ICharacterData _forceUserData;
 
         public PlayerJumpForceGenerator(IJumpingInputService jumpingInputService, IPlayerConfig config,
-            IPermanentForceAccumulator forceAccumulator)
+            IPermanentForceAccumulator forceAccumulator, ICharacterData forceUserData)
         {
             _jumpingInputService = jumpingInputService;
             _config = config;
             _forceAccumulator = forceAccumulator;
+            _forceUserData = forceUserData;
         }
 
-        public void Initialize() => _jumpingInputService.InputReceived += GenerateJumpForce;
+        public void Initialize() => _jumpingInputService.InputReceived += TryToStartJump;
 
-        public void Dispose() => _jumpingInputService.InputReceived -= GenerateJumpForce;
+        public void Dispose() => _jumpingInputService.InputReceived -= TryToStartJump;
+
+        private void TryToStartJump()
+        {
+            if (_forceUserData.IsGrounded)
+                GenerateJumpForce();
+        }
 
         private void GenerateJumpForce() => _forceAccumulator.AccumulatePermanentForce(y: _config.JumpPower);
     }
